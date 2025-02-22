148af943d873f4d06e4fed2283ea12be1a64d796
Ricardo Joasilus <rjoasilus@hotmail.com>
2025-02-21 20:27:19 -0600

    Resolved merge conflict in SyllabusGeneratorResponse.jsx

--- tools/outputs/SyllabusGeneratorResponse/SyllabusGeneratorResponse.jsx
+++ tools/outputs/SyllabusGeneratorResponse/SyllabusGeneratorResponse.jsx
@@ -23,6 +23,66 @@ import styles from './styles';
  */
 const SyllabusGeneratorResponse = () => {
   const { content: markdownContent } = useSelector((state) => state.tools.editorState.currentState);
+  const { response } = useSelector((state) => state.tools);
+
+  const renderTable = () => {
+    return (
+      <TableContainer
+        component={Paper}
+        sx={{ background: 'none', boxShadow: 'none' }}
+      >
+        <Table sx={{ background: 'none' }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Section</TableCell>
+              <TableCell>Details</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Course Title</TableCell>
+              <TableCell>{response.course_information.course_title}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Grade Level</TableCell>
+              <TableCell>{response.course_information.grade_level}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Description</TableCell>
+              <TableCell>{response.course_information.description}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Objectives</TableCell>
+              <TableCell>
+                <ul>
+                  {response.course_description_objectives.objectives.map(
+                    (objective, index) => (
+                      <li key={index}>{objective}</li>
+                    )
+                  )}
+                </ul>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={styles.lastRowCellProps}>
+                Learning Outcomes
+              </TableCell>
+              <TableCell sx={styles.lastRowCellProps}>
+                <ul>
+                  {response.course_description_objectives.intended_learning_outcomes.map(
+                    (outcome, index) => (
+                      <li key={index}>{outcome}</li>
+                    )
+                  )}
+                </ul>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
+
 
   return (
     <Fade in>
